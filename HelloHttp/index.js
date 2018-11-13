ac3de9a7c5d665d4d55a27d1bb1ed1599e6cdd36
BYAKheee <byakhee0314@gmail.com>
2018-11-13 18:12:04 +0900

    fix line of log.

--- HelloHttp/index.js
+++ HelloHttp/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('INPUT: context: ' + JSON.stringify(context));
+    context.log('INPUT: req:     ' + JSON.stringify(req));
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.log('INPUT: context: ' + JSON.stringify(context));
-        context.log('INPUT: req:     ' + JSON.stringify(req));
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
